a9e4833b1b516ee7d4d0d49cecd071fd04160387
Meera108 <141446442+Meera108@users.noreply.github.com>
2024-04-29 09:15:31 +0530

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "MR PERFECT AI";
+const doNotDelete = "MEERA RAJPUT ❤️🪽";
 /** 
 * @author NTKhang
 * @author: do not delete it
